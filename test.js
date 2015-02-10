8e814a6b84f16e6666a775a4ff5fcc938cba882a
quim <joaquim.serafim@gmail.com>
2015-02-10 13:33:28 +0000

    update dependencies, add a test for non JS Objects

--- test.js
+++ test.js
@@ -12,3 +12,11 @@ test('should return the object', function(assert) {
   assert.deepEqual(empty({a: 1, b: 2}), false);
   assert.end();
 });
+
+test('should throw  an error when the given value is not an js object',
+function(assert) {
+  assert.plan(1);
+  assert.throws(function() {
+    empty('');
+  }, {message: 'Object.keys called on non-object'});
+});
